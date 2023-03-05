58295bca650802da6eec649f624cc2d187c3debe
hbl <hbl@qq.com>
2023-03-05 23:43:07 +0800

    fixed after_clone_sh

--- action/clone_openwrt.sh
+++ action/clone_openwrt.sh
@@ -14,10 +14,10 @@
     ln -sf /workdir/openwrt $GITHUB_WORKSPACE/openwrt
 
     # after clone
-    if [ -n "$AFTER_CLONE_SH" ] && [ "$AFTER_CLONE_SH" != "false" ] && [ -f "$AFTER_CLONE_SH" ];then
+    if [ -n "$AFTER_CLONE_SH" ] && [ "$AFTER_CLONE_SH" != "false" ] && [ -f "$GITHUB_WORKSPACE/$AFTER_CLONE_SH" ];then
         (
             cd openwrt
-            echo "[Info] Run AFTER_CLONE_SH=$AFTER_CLONE_SH"
+            echo "[Info] Run AFTER_CLONE_SH=$GITHUB_WORKSPACE/$AFTER_CLONE_SH"
             bash "$AFTER_CLONE_SH"
         )
     fi
