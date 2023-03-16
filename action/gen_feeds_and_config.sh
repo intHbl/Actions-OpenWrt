93c2731efea53f4ba989379800e894c74e0ca866
hbl <hbl@qq.com>
2023-03-16 21:19:47 +0800

    1. wolfssl -> openssl \n2. use wpad-openssl

--- action/gen_feeds_and_config.sh
+++ action/gen_feeds_and_config.sh
@@ -4,15 +4,26 @@ if [ -e "$__ROOT_DIR__/$CONFIG_FILE" ] ;then
     rm "$__ROOT_DIR__/$CONFIG_FILE"
 fi
 
-if [ -e "$__ROOT_DIR__/compile/config/models/${MODEL_NAME}.conf" ];then
+modelconf="$__ROOT_DIR__/compile/config/models/${MODEL_NAME}.conf"
+modelconfadd="$modelconf.add"
+if [ -f "$modelconf" ];then
     {
-        cat "$__ROOT_DIR__/compile/config/models/${MODEL_NAME}.conf" "$__ROOT_DIR__/compile/config/"*.conf 
+        cat "$modelconf" 
+        
+        if [ -f "$modelconfadd" ];then
+            cat "$modelconfadd"
+        fi
+
+        cat "$__ROOT_DIR__/compile/config/"*.conf 
+        
     } > "$__ROOT_DIR__/$CONFIG_FILE" 
+
     
     ls -l "$__ROOT_DIR__/$CONFIG_FILE"
 
     cp "$__ROOT_DIR__/$CONFIG_FILE" "$__ROOT_DIR__/$CONFIG_FILE.2"
-
+    
+    # small
     if [ -n "$FLAG_SMALL" ] && [ "x$FLAG_SMALL" != "xnone" ] && [ "x$FLAG_SMALL" != "xfalse" ]; then
         (
             cat "$__ROOT_DIR__/compile/config/"*.conf."$FLAG_SMALL"
