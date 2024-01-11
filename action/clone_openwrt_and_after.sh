02f24cdcfbaaf185cb0b8430c71587214b96bcda
Han <hb-in@qq.com>
2024-01-12 03:40:36 +0800

    Update clone_openwrt_and_after.sh

--- action/clone_openwrt_and_after.sh
+++ action/clone_openwrt_and_after.sh
@@ -6,9 +6,15 @@
     df -hT $PWD
   
     REPO_URL="https://github.com/openwrt/openwrt"
-    REPO_BRANCH="openwrt-21.02"
-
-    b="v21.02.5"
+    # $REPO_BRANCH  not used
+    #   use tag == $b
+    if [ -z "$FLAG_NEWEST" ];then
+        REPO_BRANCH="openwrt-21.02"
+        b="v21.02.5"
+    else
+        REPO_BRANCH="openwrt-23.05"
+        b="v23.05.2"
+    fi
     # git clone --single-branch -b $REPO_BRANCH $REPO_URL  openwrt
     git clone --single-branch -b $b $REPO_URL  openwrt
     ln -sf /workdir/openwrt $GITHUB_WORKSPACE/openwrt
